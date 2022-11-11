ecce57a352ecbad6458747968443d6c0d872b195
4kang <106650699+4kang@users.noreply.github.com>
2022-11-11 11:53:27 +0000

    first commit

---
+++ index.rb
@@ -0,0 +1,17 @@
+# puts=受け取った値を文字列として出力
+puts 'Hello World!'
+
+puts 5 + 3
+puts "5 + 3"
+puts "5" + "3"
+
+puts "I" + "am" + "Sam"
+
+#to_s（整数型→文字列型へ変換）
+puts "Samの年齢は" + 27.to_s + "です"
+
+# to_i（文字列型→整数型へ変換）
+puts 100 + "200".to_i
+
+webcamp = "プログラミング学習"
+puts webcamp
